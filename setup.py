9ee73722bf4bb3699c39f310fd189bb082113721
Simon Aytes <simon@aytes.net>
2023-07-24 15:04:28 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implement a Convolutional Neural Network (CNN) to detect COVID-19 infections in chest x-rays.',
+    author='Simon Aytes',
+    license='MIT',
+)
